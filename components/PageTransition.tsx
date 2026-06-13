d3777ac1266d99050d7a00aaa1a306b5c87ad614
LaidiMohammed <hamada.laidi.14@gmail.com>
2026-06-13 19:47:23 +0100

    fix: remove mode=wait (black flash), use simultaneous 0.12s pure opacity

--- components/PageTransition.tsx
+++ components/PageTransition.tsx
@@ -7,13 +7,13 @@ export default function PageTransition({ children }: { children: React.ReactNode
   const pathname = usePathname();
 
   return (
-    <AnimatePresence mode="wait">
+    <AnimatePresence>
       <motion.div
         key={pathname}
-        initial={{ opacity: 0, y: 8 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -8 }}
-        transition={{ duration: 0.25, ease: 'easeInOut' }}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.12 }}
       >
         {children}
       </motion.div>
